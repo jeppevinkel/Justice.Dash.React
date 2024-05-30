6a34371d434847746c92789671b594f48e4a95fd
Jeppe Vinkel Beier <jevb@gfforsikring.dk>
2024-05-30 15:59:45 +0200

    Execute callbacks

--- src/Window.tsx
+++ src/Window.tsx
@@ -12,16 +12,16 @@ interface Props {
     closeCallback?: () => void,
 }
 
-function Window({title, children, sx, glass = true, maximized, fullscreen}: Props) {
+function Window({title, children, sx, glass = true, maximized, fullscreen, minimizeCallback, maximizeCallback, closeCallback}: Props) {
 
     return (
         <div className={`window ${glass ? 'glass' : ''} ${fullscreen ? 'window-full-screen' : ''} active`} style={sx}>
             <div className="title-bar">
                 <div className="title-bar-text">{title}</div>
                 <div className="title-bar-controls">
-                    <button aria-label="Minimize"></button>
-                    <button aria-label={maximized ? 'Restore' : 'Maximize'}></button>
-                    <button aria-label="Close"></button>
+                    <button aria-label="Minimize" onClick={() => {if (minimizeCallback) minimizeCallback()}}></button>
+                    <button aria-label={maximized ? 'Restore' : 'Maximize'} onClick={() => {if (maximizeCallback) maximizeCallback()}}></button>
+                    <button aria-label="Close" onClick={() => {if (closeCallback) closeCallback()}}></button>
                 </div>
             </div>
             <div className="window-body has-space has-scrollbar" style={{flex: fullscreen ? '1 1 auto' : ''}}>
