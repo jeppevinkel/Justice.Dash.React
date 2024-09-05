eaed5f258fbe079b8444973c958323e5f2fa7630
Jeppe Beier <jeppevinkel@gmail.com>
2024-09-06 00:28:25 +0200

    Update nginx config gen

--- generate-config.sh
+++ generate-config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 envsubst '$BACKEND_HOST' < /etc/nginx/conf.d/custom-nginx.template > /etc/nginx/conf.d/default.conf;
+envsubst 'BACKEND_OLD_HOST' < /etc/nginx/conf.d/custom-nginx.template > /etc/nginx/conf.d/default.conf;
 exec nginx -g "daemon off;";
